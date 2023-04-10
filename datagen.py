6c622d786d4c6a9fa1839a14356426c0c7086142
Jakub Sýkora <sykorajakku@Jakubs-MacBook-Pro.local>
2023-04-10 20:52:58 +0200

    Add initial config with data

---
+++ datagen.py
@@ -0,0 +1,213 @@
+import sys
+import json
+import random
+
+from datetime import datetime, timedelta
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+endpoint_url = "https://query.wikidata.org/sparql"
+
+query = """PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+PREFIX schema: <http://schema.org/>
+
+SELECT ?bookLabel ?genreLabel ?genreDescription ?freebase ?dop ?authorLabel ?dob ?dod (SAMPLE(?residenceName) AS ?singleResidenceName) WHERE {
+  ?book wdt:P31 wd:Q571.
+  ?book wdt:P50 ?author.
+  ?author wdt:P31 wd:Q5.
+  ?book rdfs:label ?bookLabel.
+  ?author rdfs:label ?authorLabel.
+  
+  ?author wdt:P569 ?dob.
+  
+  OPTIONAL {
+    ?author wdt:P570 ?dod.
+  }
+  
+  OPTIONAL {
+    ?book wdt:P577 ?dop.
+  }
+  
+  OPTIONAL {
+    ?book wdt:P646 ?freebase.
+  }
+  
+  OPTIONAL {
+    ?author wdt:P551 ?residence.
+    ?residence rdfs:label ?residenceName.
+    FILTER(LANG(?residenceName) = "en").
+  }
+
+  ?book wdt:P136 ?genre.
+  ?genre rdfs:label ?genreLabel.
+  ?genre schema:description ?genreDescription.
+  
+  FILTER(LANG(?genreLabel) = "en").
+  FILTER(LANG(?genreDescription) = "en").
+  FILTER(LANG(?bookLabel) = "en").
+  FILTER(LANG(?authorLabel) = "en").
+  FILTER(DATATYPE(?dob) = xsd:dateTime).
+  FILTER(!BOUND(?dod) || DATATYPE(?dod) = xsd:dateTime).
+}
+GROUP BY ?bookLabel ?genreLabel ?genreDescription ?freebase ?dop ?authorLabel ?dob ?dod
+LIMIT 10000"""
+
+libraries_query = """
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+SELECT ?libraryLabel ?countryLabel ?webpage ?isil
+WHERE {
+  ?library wdt:P31 wd:Q7075 . # ?library is an instance of (P31) Library (Q7075)
+  ?library wdt:P17 ?country.
+  ?country rdfs:label ?countryLabel.
+  ?library wdt:P856 ?webpage.
+  ?library wdt:P791 ?isil.
+  
+  FILTER(LANG(?countryLabel) = "en").
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en" . }
+}
+GROUP BY ?libraryLabel ?countryLabel ?webpage ?isil
+LIMIT 100
+"""
+
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    # TODO adjust user agent; see https://w.wiki/CX6
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+libraries_results = get_results(endpoint_url, libraries_query)
+
+genres = {}
+books = {}
+authors = {}
+
+genre_id = 1
+book_id = 1
+author_id = 1
+
+for result in results["results"]["bindings"]:
+    # Process genre
+    genre_label = result["genreLabel"]["value"]
+    genre_description = result["genreDescription"]["value"]
+    
+    if genre_label not in genres:
+        genre_key = f"genre_{genre_id}"
+        genre_id += 1
+        genres[genre_label] = {
+            "_id": genre_key,
+            "genreLabel": genre_label,
+            "genreDescription": genre_description
+        }
+        
+    # Process author
+    author_label = result["authorLabel"]["value"]
+    dob = result["dob"]["value"]
+    dod = result.get("dod", {}).get("value", None)
+    
+    if author_label not in authors:
+        author_key = f"author_{author_id}"
+        author_id += 1
+        authors[author_label] = {
+            "_id": author_key,
+            "authorLabel": author_label,
+            "dob": dob,
+            "dod": dod
+        }
+        
+    # Process book
+    book_label = result["bookLabel"]["value"]
+    dop = result.get("dop", {}).get("value", None)
+    freebase = result.get("freebase", {}).get("value", None)
+    
+    if book_label not in books:
+        book_key = f"book_{book_id}"
+        book_id += 1
+        books[book_label] = {
+            "_id": book_key,
+            "bookLabel": book_label,
+            "dop": dop,
+            "freebase": freebase,
+            "genres": [genres[genre_label]["_id"]],
+            "authors": [authors[author_label]["_id"]]
+        }
+    else:
+        books[book_label]["genres"].append(genres[genre_label]["_id"])
+        if authors[author_label]["_id"] not in books[book_label]["authors"]:
+            books[book_label]["authors"].append(authors[author_label]["_id"])
+
+libraries = []
+lease_offers = []
+item_availabilities = ["Available", "NotAvailable", "Discontinued"]
+
+# Generate 100 libraries
+idx = 1
+for result in libraries_results["results"]["bindings"]:
+    library_label = result["libraryLabel"]["value"]
+    isil = result["isil"]["value"]
+    webpage = result["webpage"]["value"]
+    country = result["countryLabel"]["value"]
+
+    libraries.append({
+        "_id": f"library_{idx + 1}",
+        "name": library_label,
+        "country": country,
+        "webpage": webpage,
+        "isil": isil
+    })
+    idx += 1
+
+# Generate 10,000 lease offers
+for i in range(1, 10001):
+    # Randomly select a book
+    book = random.choice(list(books.values()))
+
+    # Randomly select a library
+    library = random.choice(libraries)
+
+    # Randomly select an item availability
+    availability = random.choice(item_availabilities)
+
+    # Generate random lease start and end dates
+    lease_start = datetime.now() + timedelta(days=random.randint(-365, 365))
+    lease_end = lease_start + timedelta(days=random.randint(1, 365))
+
+    lease_offers.append({
+        "_id": f"lease_offer_{i}",
+        "bookId": book["_id"],
+        "libraryId": library["_id"],
+        "availability": availability,
+        "leaseStart": lease_start.isoformat(),
+        "leaseEnd": lease_end.isoformat()
+    })
+
+# Save libraries to libraries.json
+with open("libraries.json", "w") as f:
+    json.dump(libraries, f, indent=2)
+
+# Save lease_offers to lease_offers.json
+with open("lease_offers.json", "w") as f:
+    json.dump(lease_offers, f, indent=2)
+
+# Save genres to genres.json
+with open("genres.json", "w") as f:
+    json.dump(list(genres.values()), f, indent=2)
+
+# Save authors to authors.json
+with open("authors.json", "w") as f:
+    json.dump(list(authors.values()), f, indent=2)
+
+# Save books to books.json
+with open("books.json", "w") as f:
+    json.dump(list(books.values()), f, indent=2)
---
+++ init.py
@@ -0,0 +1,115 @@
+import os
+import requests
+import json
+
+from dotenv import load_dotenv
+from couchdb import Server
+
+load_dotenv()
+
+deployment_name = os.environ["COMPOSE_PROJECT_NAME"]
+coordinator_node = "0"
+additional_nodes = "1,2"
+all_nodes = f"{coordinator_node},{additional_nodes}"
+
+couchdb_user = os.environ["COUCHDB_USER"]
+couchdb_password = os.environ["COUCHDB_PASSWORD"]
+port_base = os.environ["PORT_BASE"]
+
+def make_request(url, data):
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, headers=headers, json=data)
+    return response
+
+for node_id in coordinator_node:
+    url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}{node_id}/_cluster_setup"
+    data = {
+        "action": "enable_cluster",
+        "bind_address": "0.0.0.0",
+        "username": couchdb_user,
+        "password": couchdb_user,
+        "node_count": "3"
+    }
+    make_request(url, data)
+    print("You may safely ignore the warning above.")
+
+for node_id in additional_nodes.split(","):
+    url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/_cluster_setup"
+    
+    data1 = {
+        "action": "enable_cluster",
+        "bind_address": "0.0.0.0",
+        "username": couchdb_user,
+        "password": couchdb_password,
+        "port": 5984,
+        "node_count": "3",
+        "remote_node": f"couchdb-{node_id}.{deployment_name}",
+        "remote_current_user": couchdb_user,
+        "remote_current_password": couchdb_password
+    }
+    make_request(url, data1)
+    
+    data2 = {
+        "action": "add_node",
+        "host": f"couchdb-{node_id}.{deployment_name}",
+        "port": 5984,
+        "username": couchdb_user,
+        "password": couchdb_password
+    }
+    make_request(url, data2)
+
+url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/"
+requests.get(url)
+
+url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/_cluster_setup"
+data = {"action": "finish_cluster"}
+make_request(url, data)
+
+url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/_cluster_setup"
+requests.get(url)
+
+url = f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/_membership"
+requests.get(url)
+
+print("Your cluster nodes are available at:")
+for node_id in all_nodes.split(","):
+    print(f"   http://{couchdb_user}:{couchdb_password}@localhost:{port_base}{node_id}")
+
+# Connect to CouchDB
+server = Server(f"http://{couchdb_user}:{couchdb_password}@127.0.0.1:{port_base}0/")
+
+databases = [
+    ("books", "book"),
+    ("authors", "author"),
+    ("genres", "genre"),
+    ("lease_offers", "lease_offer"),
+    ("libraries", "library"),
+]
+
+for db_name, doc_type in databases:
+    server.create(db_name)
+
+with open("books.json", "r") as f:
+    books = json.load(f)
+    for book in books:
+        server["books"].save(book)
+
+with open("authors.json", "r") as f:
+    authors = json.load(f)
+    for author in authors:
+        server["authors"].save(author)
+
+with open("genres.json", "r") as f:
+    genres = json.load(f)
+    for genre in genres:
+        server["genres"].save(genre)
+
+with open("lease_offers.json", "r") as f:
+    lease_offers = json.load(f)
+    for lease_offer in lease_offers:
+        server["lease_offers"].save(lease_offer)
+
+with open("libraries.json", "r") as f:
+    libraries = json.load(f)
+    for library in libraries:
+        server["libraries"].save(library)
